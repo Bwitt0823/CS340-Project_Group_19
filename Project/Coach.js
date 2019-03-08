eb13b66e310cc3afac42ff6c5b970995e0aa8ace
Bradford Witt <39221136+Bwitt0823@users.noreply.github.com>
2019-03-07 21:34:22 -0800

    Update Coach.js

--- Project/Coach.js
+++ Project/Coach.js
@@ -21,7 +21,7 @@ module.exports = function() {
 
 	router.DELETE('/:ID_Coach', function(req, res) {
 		var mysql = req.app.get('mysql');
-		var sql = 'DELETE FROM Coach WHERE ID_Coach = ?";
+		var sql = "DELETE FROM Coach WHERE ID_Coach = ?";
 		var inserts = [req.params.ID_Coach];
 		sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
 			if(error)
