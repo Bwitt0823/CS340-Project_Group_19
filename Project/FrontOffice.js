e9d69bef0ce26819558bcc2f7d4734bfe73a0ffa
Bradford Witt <39221136+Bwitt0823@users.noreply.github.com>
2019-03-07 21:43:07 -0800

    Update FrontOffice.js

--- Project/FrontOffice.js
+++ Project/FrontOffice.js
@@ -4,17 +4,36 @@ module.exports = function() {
 
 
 
-router.POST('/FrontOffice', function(req, res) {
-	var mysql = req.app.get('mysql');
-	var sql = "INSERT INTO FrontOffice (First_Name, Last_Name, Role, Team) VALUES (?, ?, ?, ?)";
-	var inserts = [req.body.First_Name, req.body.Last_Name, req.body.Role, req.body.Team];
-	sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
-		if(error) {
-			res.write(JSON.stringify(error));
-			res.end();
-		}
-		else {
-			res.redirect('/FrontOffice');
-		}
+	router.POST('/FrontOffice', function(req, res) {
+		var mysql = req.app.get('mysql');
+		var sql = "INSERT INTO FrontOffice (First_Name, Last_Name, Role, Team) VALUES (?, ?, ?, ?)";
+		var inserts = [req.body.First_Name, req.body.Last_Name, req.body.Role, req.body.Team];
+		sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
+			if(error) {
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+			else {
+				res.redirect('/FrontOffice');
+			}
+		});
 	});
-});
+
+	router.DELETE('/FrontOffice:ID_FrontOffice', function(req, res) {
+		var mysql = req.app.get('mysql');
+		var sql = "DELETE FROM FrontOffice WHERE ID_FrontOffice = ?";
+		var inserts = [req.params.ID_FrontOffice];
+		sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
+			if(error)
+				res.write(JSON.stringify(error));
+				res.status(400);
+				res.end();
+			}
+			else {
+				res.status(202).end();
+			}
+		})
+	});
+
+	return router;
+}();
