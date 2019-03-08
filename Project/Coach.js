a602cfa0c91217d4f82b3100d7edb24678414c81
Bradford Witt <39221136+Bwitt0823@users.noreply.github.com>
2019-03-07 21:33:58 -0800

    Update Coach.js

--- Project/Coach.js
+++ Project/Coach.js
@@ -4,17 +4,36 @@ module.exports = function() {
 
 
 
-router.POST('/Coach', function(req, res) {
-	var mysql = req.app.get('mysql');
-	var sql = "INSERT INTO Coach (First_Name, Last_Name, Team) VALUES (?, ?, ?)";
-	var inserts = [req.body.First_Name, req.body.Last_Name, req.body.Team];
-	sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
-		if(error) {
-			res.write(JSON.stringify(error));
-			res.end();
-		}
-		else {
-			res.redirect('/Coach');
-		}
+	router.POST('/Coach', function(req, res) {
+		var mysql = req.app.get('mysql');
+		var sql = "INSERT INTO Coach (First_Name, Last_Name, Team) VALUES (?, ?, ?)";
+		var inserts = [req.body.First_Name, req.body.Last_Name, req.body.Team];
+		sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
+			if(error) {
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+			else {
+				res.redirect('/Coach');
+			}
+		});
 	});
-});
+
+	router.DELETE('/:ID_Coach', function(req, res) {
+		var mysql = req.app.get('mysql');
+		var sql = 'DELETE FROM Coach WHERE ID_Coach = ?";
+		var inserts = [req.params.ID_Coach];
+		sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
+			if(error)
+				res.write(JSON.stringify(error));
+				res.status(400);
+				res.end();
+			}
+			else {
+				res.status(202).end();
+			}
+		})
+	});
+
+	return router;
+}();
