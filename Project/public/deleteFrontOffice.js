e4ef0e168617acfbb5fe487e3605dd01986d6cb7
Bradford Witt <39221136+Bwitt0823@users.noreply.github.com>
2019-03-07 20:54:42 -0800

    Update deleteFrontOffice.js

--- Project/public/deleteFrontOffice.js
+++ Project/public/deleteFrontOffice.js
@@ -1,6 +1,6 @@
 function deleteFrontOffice(ID_Front_Office) {
   $.ajax({
-    url: '/frontOffice/' + ID_Front_Office,
+    url: '/FrontOffice/' + ID_Front_Office,
     type: 'DELETE',
     success: function(result){
       window.location.reload(true);
