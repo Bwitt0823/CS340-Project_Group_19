0b8fae5cb60b9fe9c22018311df56cad274bdbed
Bradford Witt <39221136+Bwitt0823@users.noreply.github.com>
2019-03-07 20:43:33 -0800

    Create deleteCoach.js

---
+++ Project/public/deleteCoach.js
@@ -0,0 +1,9 @@
+function deleteCoach(ID_Coach) {
+  $.ajax({
+    url: '/Coach/' + ID_Coach,
+    type: 'DELETE',
+    success: function(result){
+      window.location.reload(true);
+    }
+  })
+};
