07c5d6590aadb23c70509cf61a8dd4bc355315ca
Bradford Witt <39221136+Bwitt0823@users.noreply.github.com>
2019-03-14 21:10:39 -0700

    Update Coach.js

--- Project/routes/Coach.js
+++ Project/routes/Coach.js
@@ -6,8 +6,8 @@ module.exports = function() {
 	router.get('/', function(req, res) {
 		const context = {};
 		context.jsscripts = [];
-		context.title = "Home";
-		res.render('index', context);
+		context.title = "Coach";
+		res.render('Coach', context);
 	});
 
 	/*router.POST('/Coach', function(req, res) {
