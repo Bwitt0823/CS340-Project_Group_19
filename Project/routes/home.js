22b0f6c8e9ac21b438b1ce1ef3211f6359f923c2
Bradford Witt <39221136+Bwitt0823@users.noreply.github.com>
2019-03-14 20:43:37 -0700

    Update home.js

--- Project/routes/home.js
+++ Project/routes/home.js
@@ -1,1 +1,15 @@
 module.exports = function() {
+	const express = require('express');
+	const router = express.Router();
+	
+	//Render Home Page
+	router.get('/', function(req, res) {
+		const context = {};
+		context.jsscripts = [];
+		context.title = "Home";
+		res.render('index', context);
+	});
+
+	
+return router;
+}();
