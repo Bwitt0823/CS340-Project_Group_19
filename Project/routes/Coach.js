b2fa9461ede97eff85cd2a37b05389e9544d77bf
Bradford Witt <39221136+Bwitt0823@users.noreply.github.com>
2019-03-16 12:28:58 -0700

    Update Coach.js

--- Project/routes/Coach.js
+++ Project/routes/Coach.js
@@ -102,7 +102,7 @@ module.exports = function() {
 		var sql = "SELECT First_Name, Last_Name FROM `Coach` WHERE ID_Coach=?";
 		var inserts = [req.params.cid];
 		sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
-			if(errror) {
+			if(error) {
 				return res.render('404');
 			};
 		});
