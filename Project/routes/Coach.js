d1f9d33f0cd997b1b04c97860bbfceeedc678fd3
Alex Swider <alextswider@gmail.com>
2019-03-16 14:24:48 -0700

    Update Coach.js

--- Project/routes/Coach.js
+++ Project/routes/Coach.js
@@ -128,6 +128,7 @@ module.exports = function() {
 		console.log(req.params.cid)
 		var sql = "UPDATE `Coach` SET First_Name = ?, Last_Name = ?, Team = ? WHERE ID_Coach = ?";
 		var inserts = [req.body.New_First_Name, req.body.New_Last_Name, req.body.New_Team, req.params.cid];
+		getCoachUpdate(req, res, mysql, context, complete);
 		sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
 			if(error) {
 				console.log("There was an error in posting")
