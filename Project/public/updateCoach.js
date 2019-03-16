4b801c9c4818ea8e2cc40e193ad7476a81bbedf3
Alex Swider <alextswider@gmail.com>
2019-03-16 12:23:00 -0700

    Update updateCoach.js

--- Project/public/updateCoach.js
+++ Project/public/updateCoach.js
@@ -1,1 +1,10 @@
-
+function updateCoach(cid){
+    $.ajax({
+        url: '/UpdateCoach/' + cid,
+        type: 'PUT',
+        data: $('UpdateCoach').serialize(),
+        success: function(result){
+            window.location.replace("./");
+        }
+    })
+};
