dbf11768103fe8d2798ac4130bbc047d3a43608a
Alex Swider <alextswider@gmail.com>
2019-03-16 14:27:10 -0700

    Update Coach.js

--- Project/routes/Coach.js
+++ Project/routes/Coach.js
@@ -124,6 +124,7 @@ module.exports = function() {
 	//Update Coach
 	router.post('/UpdateCoach/:cid', function(req, res) {
 		var mysql = req.app.get('mysql');
+		const context = {};
 		console.log(req.body)
 		console.log(req.params.cid)
 		var sql = "UPDATE `Coach` SET First_Name = ?, Last_Name = ?, Team = ? WHERE ID_Coach = ?";
