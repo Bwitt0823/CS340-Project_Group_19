34faacf6008473c8b5d026f30f401b5d79695ff9
Alex Swider <alextswider@gmail.com>
2019-03-17 21:28:43 -0700

    Update filterPlayer.js

--- Project/public/filterPlayer.js
+++ Project/public/filterPlayer.js
@@ -1,6 +1,6 @@
 function filterPlayerbyTeam() {
     //get the id of the selected homeworld from the filter dropdown
-    var Team_id = document.getElementById('Team_filter').value
+    var Team_id = document.getElementById('filter_Player').value
     //construct the URL and redirect to it
     window.location = '/people/filter/' + encodeURI(Team_id)
 }
