1819915d1fa77059c7e8400cc839c50d6c2e8c00
Alex Swider <alextswider@gmail.com>
2019-03-17 21:49:19 -0700

    Update Player.js

--- Project/routes/Player.js
+++ Project/routes/Player.js
@@ -50,7 +50,7 @@ module.exports = function() {
 	});
 	
       function getPlayerbyTeam(req, res, mysql, context, complete){
-      var query = "SELECT ID_Player AS pid, First_Name, Last_Name, College, Number, Team FROM `Player` ON Team = Team.Abbreviation WHERE Team LIKE " mysql.pool.escape(req.params.s+'%');
+      var query = "SELECT ID_Player AS pid, First_Name, Last_Name, College, Number, Team FROM `Player` ON Team = Team.Abbreviation WHERE Team LIKE " + mysql.pool.escape(req.params.s+'%');
       console.log(query)
       
       mysql.pool.query(query, function(error, results, fields){
