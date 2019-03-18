8860f4eb7d08d50de78a41da1b7f0ee7cd3268c3
Alex Swider <alextswider@gmail.com>
2019-03-17 21:00:12 -0700

    Update Player.js

--- Project/routes/Player.js
+++ Project/routes/Player.js
@@ -52,7 +52,7 @@ module.exports = function() {
       function getPlayerbyTeam(req, res, mysql, context, complete){
       var query = "SELECT ID_Player AS pid, First_Name, Last_Name, College, Number, Team FROM `Player` ON Team = Team.Abbreviation WHERE Team = ?";
       console.log(req.params)
-      var inserts = [req.params.Team]
+      var inserts = [req.params.Team_Abb]
       mysql.pool.query(query, inserts, function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
