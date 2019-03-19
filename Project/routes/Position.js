d0fcbbb09f490be81085da52b3462aa009a0d475
Bradford Witt <39221136+Bwitt0823@users.noreply.github.com>
2019-03-18 21:47:00 -0700

    Update Position.js

--- Project/routes/Position.js
+++ Project/routes/Position.js
@@ -1,1 +1,44 @@
-
+module.exports = function() {
+	var express = require('express');
+	var router = express.Router();
+	
+	function getPosition(res, mysql, context, complete) {
+		var sql = "SELECT * FROM `Position` WHERE 1";
+		mysql.pool.query(sql, function(error, results, fields) {
+			if(error) {
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+			context.Position = results;
+			complete();
+	};
+						 
+	function getPlayer(res, mysql, context, complete) {
+		var sql = "SELECT * FROM `Player` WHERE 1";
+		mysql.pool.query(sql, function(error, results, fields) {
+			if(error) {
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+			context.Player = results;
+			complete();
+	};
+						 
+	router.get('/', function(req, res) {
+			var callbackCount = 0;
+			const contect = {};
+			context.title = "Position";
+			context.jsscripts = [];
+			var mysql = req.app.get('mysql');
+			getPosition(res, mysql, context, complete);
+			function complete() {
+				callbackCount++;
+				if(callbackCount >= 1) {
+					console.log(context.Position);
+					res.render('Position', context);
+				}
+			}
+		});
+	
+	return router;
+}();
