742855653f68959d900e52f6049af0fcd4b826a9
Bradford Witt <39221136+Bwitt0823@users.noreply.github.com>
2019-03-19 12:19:01 -0700

    Update Data_Definitions.sql

--- Project/Data_Definitions.sql
+++ Project/Data_Definitions.sql
@@ -1,117 +1,341 @@
--- Project Group 19
--- Alexander Swider & Bradford Witt
--- February 10, 2019
--- Data Definition Queries
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: classmysql.engr.oregonstate.edu:3306
+-- Generation Time: Mar 19, 2019 at 12:18 PM
+-- Server version: 10.1.22-MariaDB
+-- PHP Version: 7.0.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-DROP TABLE IF EXISTS Player_Position;
-DROP TABLE IF EXISTS Coach_Position;
-DROP TABLE IF EXISTS Position;
-DROP TABLE IF EXISTS Player;
-DROP TABLE IF EXISTS Coach;
-DROP TABLE IF EXISTS Front_Office;
-DROP TABLE IF EXISTS Team;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Create Team Table
-CREATE TABLE Team (
-    Abbreviation char(4) PRIMARY KEY,
-    Name char(25),
-    City char(50),
-    State char(50),
-    Country char(50),
-    Stadium char(50)
-);
+--
+-- Database: `cs340_swidera`
+--
 
--- Create Front_Office Table
-CREATE TABLE Front_Office (
-    ID_Front_Office int AUTO_INCREMENT PRIMARY KEY,
-    First_Name char(50),
-    Last_Name char(50),
-    Role char(50),
-    Team char(4),
-    FOREIGN KEY Team_Front_Office (Team)
-    	REFERENCES Team (Abbreviation)
-);
+-- --------------------------------------------------------
 
--- Create Coach Table
-CREATE TABLE Coach (
-    ID_Coach int AUTO_INCREMENT PRIMARY KEY,
-    First_Name char(50),
-    Last_Name char(50),
-    Team char(4),
-    FOREIGN KEY Team_Coach (Team)
-    	REFERENCES Team (Abbreviation)
-);
+--
+-- Table structure for table `Coach`
+--
 
--- Create Player Table
-CREATE TABLE Player (
-    ID_Player int AUTO_INCREMENT PRIMARY KEY,
-    First_Name char(50),
-    Last_Name char(50),
-    College char(50),
-    Number char(2),
-    Team char(4),
-    FOREIGN KEY Team_Player (Team)
-    	REFERENCES Team (Abbreviation)
-);
+CREATE TABLE `Coach` (
+  `ID_Coach` int(11) NOT NULL,
+  `First_Name` char(50) DEFAULT NULL,
+  `Last_Name` char(50) DEFAULT NULL,
+  `Team` char(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Create Position Table
-CREATE TABLE Position (
-    ID_Position int AUTO_INCREMENT PRIMARY KEY,
-    Position_Type char(15),
-    Position_Group char(25)
-);
+--
+-- Dumping data for table `Coach`
+--
 
--- Create Player_Position Table
-CREATE TABLE Player_Position (
-    ID_Player int,
-    ID_Position int,
-    PRIMARY KEY (ID_Player, ID_Position),
-    FOREIGN KEY Player_ID (ID_Player)
-    	REFERENCES Player (ID_Player) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY pPosition_ID (ID_Position)
-    	REFERENCES Position (ID_Position)
-);
+INSERT INTO `Coach` (`ID_Coach`, `First_Name`, `Last_Name`, `Team`) VALUES
+(1, 'Bill', 'Belichick', 'NE'),
+(2, 'Sean', 'McVay', 'LAR');
 
--- Create Coach_Position Table
-CREATE TABLE Coach_Position (
-    ID_Coach int,
-    ID_Position int,
-    PRIMARY KEY (ID_Coach, ID_Position),
-    FOREIGN KEY Coach_ID (ID_Coach)
-    	REFERENCES Coach (ID_Coach) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY cPosition_ID (ID_Position)
-    	REFERENCES Position (ID_Position)
-);
+-- --------------------------------------------------------
 
--- Insert Sample Team
-INSERT INTO Team 
-	(Abbreviation, Name, City, State, Country, Stadium)
-VALUES ('NE', 'Patriots', 'New England', 'Massachusetts', 'United States', 'Gillette Stadium'),
-		('LAR', 'Rams', 'Los Angeles', 'California', 'United States', 'Los Angeles Memorial Coliseum');
+--
+-- Table structure for table `Coach_Position`
+--
 
--- Insert Sample Player
-INSERT INTO Player
-	(First_Name, Last_Name, College, Number, Team)
-VALUES ('Tom', 'Brady', 'Michigan', '12', 'NE'),
-		('Aaron', 'Donald', 'Pittsburgh', '99', 'LAR');
+CREATE TABLE `Coach_Position` (
+  `ID_Coach` int(11) NOT NULL,
+  `ID_Position` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Insert Sample Coach
-INSERT INTO Coach
-	(First_Name, Last_Name, Team)
-VALUES ('Bill', 'Belichick', 'NE'),
-		('Sean', 'McVay', 'LAR');
+-- --------------------------------------------------------
 
--- Insert Sample Front Office Members
-INSERT INTO Front_Office 
-	(First_Name, Last_Name, Role, Team)
-VALUES ('Robert', 'Craft', 'Owner', 'NE'),
-		('Bill', 'Belichick', 'General Manager', 'NE'),
-		('Stan', 'Kroenke', 'Owner', 'LAR'),
-		('Les', 'Snead', 'General Manager', 'LAR');
+--
+-- Table structure for table `diagnostic`
+--
 
--- Insert Sample Positions
-INSERT INTO Position
-	(Position_Type, Position_Group)
-VALUES ('Offense', 'Quarterback'),
-		('Defense', 'Defensive Tackle');
+CREATE TABLE `diagnostic` (
+  `id` int(11) NOT NULL,
+  `text` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `diagnostic`
+--
+
+INSERT INTO `diagnostic` (`id`, `text`) VALUES
+(1, 'MySQL is Working!');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Front_Office`
+--
+
+CREATE TABLE `Front_Office` (
+  `ID_Front_Office` int(11) NOT NULL,
+  `First_Name` char(50) DEFAULT NULL,
+  `Last_Name` char(50) DEFAULT NULL,
+  `Role` char(50) DEFAULT NULL,
+  `Team` char(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Front_Office`
+--
+
+INSERT INTO `Front_Office` (`ID_Front_Office`, `First_Name`, `Last_Name`, `Role`, `Team`) VALUES
+(1, 'Robert', 'Craft', 'Owner', 'NE'),
+(2, 'Bill', 'Belichick', 'General Manager', 'NE'),
+(3, 'Stan', 'Kroenke', 'Owner', 'LAR');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Player`
+--
+
+CREATE TABLE `Player` (
+  `ID_Player` int(11) NOT NULL,
+  `First_Name` char(50) DEFAULT NULL,
+  `Last_Name` char(50) DEFAULT NULL,
+  `College` char(50) DEFAULT NULL,
+  `Number` char(2) DEFAULT NULL,
+  `Team` char(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Player`
+--
+
+INSERT INTO `Player` (`ID_Player`, `First_Name`, `Last_Name`, `College`, `Number`, `Team`) VALUES
+(1, 'Tom', 'Brady', 'Michigan', '12', 'NE'),
+(2, 'Aaron', 'Donald', 'Pittsburgh', '99', 'LAR'),
+(3, 'Aaron', 'Rodgers', 'California', '12', 'GB'),
+(4, 'Davante', 'Adams', 'Fresno State', '17', 'GB'),
+(5, 'Philip', 'Rivers', 'North Carolina State', '17', 'LAC'),
+(6, 'Russell', 'Okung', 'Oklahoma State', '76', 'LAC'),
+(7, 'Casey', 'Hayward', 'Vanderbilt', '26', 'LAC'),
+(8, 'Rashaad', 'Penny', 'San Diego State', '20', 'SEA'),
+(9, 'Shaquill', 'Griffin', 'University of Central Florida', '26', 'SEA');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Player_Position`
+--
+
+CREATE TABLE `Player_Position` (
+  `ID_Player` int(11) NOT NULL,
+  `ID_Position` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Player_Position`
+--
+
+INSERT INTO `Player_Position` (`ID_Player`, `ID_Position`) VALUES
+(1, 1),
+(2, 2),
+(4, 5),
+(5, 1),
+(8, 3),
+(8, 18);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Position`
+--
+
+CREATE TABLE `Position` (
+  `ID_Position` int(11) NOT NULL,
+  `Position_Type` char(15) DEFAULT NULL,
+  `Position_Group` char(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Position`
+--
+
+INSERT INTO `Position` (`ID_Position`, `Position_Type`, `Position_Group`) VALUES
+(1, 'Offense', 'Quarterback'),
+(2, 'Defense', 'Defensive Tackle'),
+(3, 'Offense', 'Running Back'),
+(4, 'Offense', 'Full Back'),
+(5, 'Offense', 'Wide Receiver'),
+(6, 'Offense', 'Tight End'),
+(7, 'Offense', 'Tackle'),
+(8, 'Offense', 'Guard'),
+(9, 'Offense', 'Center'),
+(10, 'Defense', 'Defensive End'),
+(11, 'Defense', 'Outside Linebacker'),
+(12, 'Defense', 'Inside Linebacker'),
+(13, 'Defense', 'Cornerback'),
+(14, 'Defense', 'Safety'),
+(15, 'Special Teams', 'Kicker'),
+(16, 'Special Teams', 'Punter'),
+(17, 'Special Teams', 'Long Snapper'),
+(18, 'Special Teams', 'Returner');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Team`
+--
+
+CREATE TABLE `Team` (
+  `Abbreviation` char(4) NOT NULL,
+  `Name` char(25) DEFAULT NULL,
+  `City` char(50) DEFAULT NULL,
+  `State` char(50) DEFAULT NULL,
+  `Country` char(50) DEFAULT NULL,
+  `Stadium` char(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Team`
+--
+
+INSERT INTO `Team` (`Abbreviation`, `Name`, `City`, `State`, `Country`, `Stadium`) VALUES
+('GB', 'Packers', 'Green Bay', 'Wisconsin', 'United States', 'Lambeau Field'),
+('LAC', 'Chargers', 'Carson', 'California', 'United States', 'Dignity Health Sports Park'),
+('LAR', 'Rams', 'Los Angeles', 'California', 'United States', 'Los Angeles Memorial Coliseum'),
+('NE', 'Patriots', 'New England', 'Massachusetts', 'United States', 'Gillette Stadium'),
+('SEA', 'Seahawks', 'Seattle', 'Washington', 'United States', 'CenturyLink Field');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Coach`
+--
+ALTER TABLE `Coach`
+  ADD PRIMARY KEY (`ID_Coach`),
+  ADD KEY `Team_Coach` (`Team`);
+
+--
+-- Indexes for table `Coach_Position`
+--
+ALTER TABLE `Coach_Position`
+  ADD PRIMARY KEY (`ID_Coach`,`ID_Position`),
+  ADD KEY `cPosition_ID` (`ID_Position`);
+
+--
+-- Indexes for table `diagnostic`
+--
+ALTER TABLE `diagnostic`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Front_Office`
+--
+ALTER TABLE `Front_Office`
+  ADD PRIMARY KEY (`ID_Front_Office`),
+  ADD KEY `Team_Front_Office` (`Team`);
+
+--
+-- Indexes for table `Player`
+--
+ALTER TABLE `Player`
+  ADD PRIMARY KEY (`ID_Player`),
+  ADD KEY `Team_Player` (`Team`);
+
+--
+-- Indexes for table `Player_Position`
+--
+ALTER TABLE `Player_Position`
+  ADD PRIMARY KEY (`ID_Player`,`ID_Position`) USING BTREE;
+
+--
+-- Indexes for table `Position`
+--
+ALTER TABLE `Position`
+  ADD PRIMARY KEY (`ID_Position`);
+
+--
+-- Indexes for table `Team`
+--
+ALTER TABLE `Team`
+  ADD PRIMARY KEY (`Abbreviation`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Coach`
+--
+ALTER TABLE `Coach`
+  MODIFY `ID_Coach` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `diagnostic`
+--
+ALTER TABLE `diagnostic`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `Front_Office`
+--
+ALTER TABLE `Front_Office`
+  MODIFY `ID_Front_Office` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `Player`
+--
+ALTER TABLE `Player`
+  MODIFY `ID_Player` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `Position`
+--
+ALTER TABLE `Position`
+  MODIFY `ID_Position` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Coach`
+--
+ALTER TABLE `Coach`
+  ADD CONSTRAINT `Team_Coach` FOREIGN KEY (`Team`) REFERENCES `Team` (`Abbreviation`);
+
+--
+-- Constraints for table `Coach_Position`
+--
+ALTER TABLE `Coach_Position`
+  ADD CONSTRAINT `Coach_ID` FOREIGN KEY (`ID_Coach`) REFERENCES `Coach` (`ID_Coach`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cPosition_ID` FOREIGN KEY (`ID_Position`) REFERENCES `Position` (`ID_Position`);
+
+--
+-- Constraints for table `Front_Office`
+--
+ALTER TABLE `Front_Office`
+  ADD CONSTRAINT `Team_Front_Office` FOREIGN KEY (`Team`) REFERENCES `Team` (`Abbreviation`);
+
+--
+-- Constraints for table `Player`
+--
+ALTER TABLE `Player`
+  ADD CONSTRAINT `Team_Player` FOREIGN KEY (`Team`) REFERENCES `Team` (`Abbreviation`);
+
+--
+-- Constraints for table `Player_Position`
+--
+ALTER TABLE `Player_Position`
+  ADD CONSTRAINT `Player_ID` FOREIGN KEY (`ID_Player`) REFERENCES `Player` (`ID_Player`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
